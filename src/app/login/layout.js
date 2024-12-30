bee475c2c079c130cc7858dcd5d589306635ad2e
chirag31045 <chiragkumawat457@gmail.com>
2024-12-30 18:51:44 +0530

    conditional layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,10 +1,16 @@
+'use client'
 import Link from "next/link";
 import './login.css'
+import { usePathname } from "next/navigation";
 
 export default function Layout({children}){
+    const pathName=usePathname();
+    console.log(pathName);
     return(
         <div>
-            <ul className="login-menu">
+            {
+                pathName!="/login/loginteacher"?
+                <ul className="login-menu">
                 <li>
                     <h4>Login Navbar</h4>
                 </li>
@@ -18,6 +24,10 @@ export default function Layout({children}){
                     <Link href='/login/loginteacher'>Teacher Login</Link>
                 </li>
             </ul>
+            :<Link href={"/login"}>Go To Main Login Page</Link>
+
+            }
+            
             {children}
         </div>
     )
